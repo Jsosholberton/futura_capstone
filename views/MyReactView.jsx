5ea5de211423b437137b74727dec2765ef77aa56
jegomezV <jegomezvalencia@gmail.com>
2023-10-17 13:50:28 -0500

    spaces

--- views/MyReactView.jsx
+++ views/MyReactView.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 function MyReactView(props) {
   const { newData } = props;
+
   const divStyle = {
     padding: '1rem',
     border: '1px solid #ccc',
     backgroundColor: '#f0f0f0',
     alignItems: 'center',
   };
+
   const h1Style = {
     fontSize: '1.25rem',
     fontWeight: 'bold',
     color: '#007bff',
     marginBottom: '0.5rem',
   };
+
   const pStyle = {
     fontSize: '1rem',
     color: '#333',
     marginBottom: '0.5rem',
   };
+
   return (
     <div style={divStyle}>
       <h1 style={h1Style}>The information of {newData.properties.Nombre.title[0].text.content} has updated success</h1>
@@ -31,4 +35,5 @@ function MyReactView(props) {
     </div>
   );
 }
+
 export default MyReactView;
