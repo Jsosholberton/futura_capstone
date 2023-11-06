453a7ef20701adb29228809d550173e54a46449b
CharlineMosquera <char951144@gmail.com>
2023-11-06 15:50:37 -0500

    Documenting the code with the JSDoc style

--- backend/controllers/CompanyController.js
+++ backend/controllers/CompanyController.js
@@ -1,6 +1,5 @@
 import { Client } from "@notionhq/client";
 import dotenv from "dotenv";
-import fs from "fs";
 import nodemailer from "nodemailer";
 
 
@@ -10,15 +9,26 @@ const notion = new Client({
     auth: process.env.NOTION_TOKEN,
   });
 
+
+/**
+ * Controller for business and email management.
+ * @class
+ */
 class CompanyController {
 
-    // Static method to send emails to companies with copies of candidates
+    /**
+     * Static method to send emails to companies with copies of candidates
+     * @param {Object} req - Purpose of express request.
+     * @param {Object} res - Express response object.
+     * @returns {void}
+     * @throws {Error} If an error occurs during the process.
+     */
     static async sendEmail(req, res){
         const { id } = req.params;
-        
         const copies = [];
         
         try {
+            // Get candidate information from Notion.
             const element = await notion.pages.retrieve({
                 page_id: id,
             });
@@ -31,21 +41,32 @@ class CompanyController {
                 copies.push(tmp.properties["Copy Final"].formula.string);
             }
 
+            // Concatenate the candidate copies.
             const stringCopies = copies.join("\n-----------------------------------------------------------\n");
+
+            // Send the company copies of the candidates by email
             CompanyController.sendCandidateCopies(stringCopies, element)
             res.status(200).send("Funciona")
+
         } catch (error) {
             console.log(error)
         }
     }
 
-    // Send the email
+    /**
+     * Static method for sending candidate copy emails to a specific company.
+     * @param {string} copies - Copies of candidates in text format.
+     * @param {Object} company - Object that represents the company.
+     * @returns {void}
+     * @throws {Error} If there is an error during the process.
+     */
     static async sendCandidateCopies(copies, company) {
 
         const email = company.properties.Correo.email;
         const contacto = company.properties.Contacto.rich_text[0].text.content;
         const name = company.properties.Name.title[0].text.content;
       
+        // Configures the email transport.
         const transporter = nodemailer.createTransport({
           host: "smtp.gmail.com",
           port: 465,
@@ -56,6 +77,7 @@ class CompanyController {
           },
       });
       
+        // Send the email with the candidates' copies.
         const info = await transporter.sendMail({
           from: "enviocorreopdf@gmail.com",
           to: email,
@@ -67,4 +89,5 @@ class CompanyController {
       }
 }
 
+// Exports the CompanyController class so that it can be used in other modules.
 export default CompanyController;
