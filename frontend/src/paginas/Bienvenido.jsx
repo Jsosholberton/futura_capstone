ac4e6d775a6e052bcb20f4b0000f5eca0fd02ec1
rubio91147 <rubendariofb@gmail.com>
2023-11-09 20:05:24 -0500

    view welcome

--- frontend/src/paginas/Bienvenido.jsx
+++ frontend/src/paginas/Bienvenido.jsx
@@ -11,9 +11,12 @@ const Proyectos = () => {
 
   return (
     <>
-     <div>
-      Bienvenido
-     </div>
+    <div className="py-14 rounded-xl p-10  shadow w-1/2 mx-auto backdrop-blur-sm bg-white/10">
+     <h1 className="font-light-green text-5xl font-sans">
+        <b>Welcome <br></br> to </b><span className="text-white">auto</span><span className="text-[#008065]">&lt;</span><span className="text-[#7B50C2]">mate</span><span className="text-[#008065]">&gt;</span>
+        
+     </h1> 
+    </div>
     </>
   );
 };
